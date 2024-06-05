b9914f9552096529edd95c6a90fac79d00fff51a
Crowdbotics <team@crowdbotics.com>
2024-06-05 21:25:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ahia-48286",
+  url: "https://ahia-48286.botics.co",
+  api: "https://ahia-48286.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
